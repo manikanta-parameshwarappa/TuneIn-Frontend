1a56fa09950a12ee4a86828b3408c927bee00bb5
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-03-18 23:35:52 +0530

    clear-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React from "react";
-import { useAuth } from "./hooks/useAuth";
-import SignupForm from "./components/SignupForm";
-import LoginForm from "./components/LoginForm";
-import ProtectedPage from "./components/ProtectedPage";
-
 function App() {
-  const { user, signup, login, logout, loading } = useAuth();
-
-  if (loading) return <p>Loading...</p>;
-
   return (
     <div>
-      {!user ? (
-        <>
-          <SignupForm onSignup={signup} />
-          <LoginForm onLogin={login} />
-        </>
-      ) : (
-        <>
-          <ProtectedPage user={user} />
-          <button onClick={logout}>Logout</button>
-        </>
-      )}
+      Stay TuneIn
     </div>
   );
 }
